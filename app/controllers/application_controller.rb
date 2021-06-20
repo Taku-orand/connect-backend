16b4fc827d4e9ce394f46ec732d1fa04f2ab6246
matallow <akoegpjmdm@icloud.com>
2021-06-20 16:21:49 +0900

    Bodyの処理を移動

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,9 +10,4 @@ class ApplicationController < ActionController::API
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
-
-  private
-  def receiveBody
-   JSON.parse(request.body.read, {:symbolize_names => true})
-  end
 end
