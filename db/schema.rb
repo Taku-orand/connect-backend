47f56c3259675000e2fd9f51113ac24007f72662
matallow <akoegpjmdm@icloud.com>
2021-06-28 11:53:04 +0900

    likeにquestion_idをもたせる

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_26_095632) do
+ActiveRecord::Schema.define(version: 2021_06_28_025200) do
 
   create_table "answers", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "content"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2021_06_26_095632) do
     t.integer "count"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "question_id"
   end
 
   create_table "questions", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -38,7 +39,6 @@ ActiveRecord::Schema.define(version: 2021_06_26_095632) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id"
     t.boolean "solved"
-    t.integer "like_id"
     t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
