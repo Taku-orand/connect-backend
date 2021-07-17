b20052dd95c2d738a1c74a4a0595dc7026592f54
matallow <akoegpjmdm@icloud.com>
2021-07-17 18:03:07 +0900

    signupのとき、８文字未満のパスワードを拒否する signupのとき、パスワードと確認パスワードが不一致の時拒否する

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
 
   validates :email, presence: true
   validates :email, uniqueness: true
+  validates :password, presence: true, length: { minimum: 8 }
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 
   has_many :questions, dependent: :destroy
