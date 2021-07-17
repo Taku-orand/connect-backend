db3398250d4ae90b2a95d48dffe9116e0e6e4158
Takuya <uoastudent182@gmail.com>
2021-07-17 17:40:54 +0900

    テストtag seed作成

---
+++ db/fixtures/tag.rb
@@ -0,0 +1,205 @@
+Tag.seed do |t|
+  t.name = "１年"
+end
+
+Tag.seed do |t|
+  t.name = "２年"
+end
+
+
+Tag.seed do |t|
+  t.name = "３年"
+end
+
+Tag.seed do |t|
+  t.name = "４年"
+end
+
+Tag.seed do |t|
+  t.name = "コンピュータシステム概論"
+end
+
+Tag.seed do |t|
+  t.name = "線形代数"
+end
+
+Tag.seed do |t|
+  t.name = "微積"
+end
+
+Tag.seed do |t|
+  t.name = "情報倫理"
+end
+
+Tag.seed do |t|
+  t.name = "力学"
+end
+
+Tag.seed do |t|
+  t.name = "電磁気学"
+end
+
+Tag.seed do |t|
+  t.name = "プログラミング入門"
+end
+
+Tag.seed do |t|
+  t.name = "試験・テスト"
+end
+
+
+Tag.seed do |t|
+  t.name = "プログラミングC"
+end
+
+Tag.seed do |t|
+  t.name = "プログラミングJava"
+end
+
+Tag.seed do |t|
+  t.name = "アルゴリズムとデータ構造I"
+end
+
+Tag.seed do |t|
+  t.name = "アルゴリズムとデータ構造II"
+end
+
+Tag.seed do |t|
+  t.name = "コンピュータリテラシー"
+end
+
+Tag.seed do |t|
+  t.name = "サークル"
+end
+
+Tag.seed do |t|
+  t.name = "学祭"
+end
+
+Tag.seed do |t|
+  t.name = "サークル自治会"
+end
+
+Tag.seed do |t|
+  t.name = "学生会執行部"
+end
+
+Tag.seed do |t|
+  t.name = "コンピュータシステム概論"
+end
+
+Tag.seed do |t|
+  t.name = "フーリエ解析"
+end
+
+Tag.seed do |t|
+  t.name = "確率統計学"
+end
+
+Tag.seed do |t|
+  t.name = "マルチメディアシステム概論"
+end
+
+Tag.seed do |t|
+  t.name = "オペレーティングシステム概論・OS"
+end
+
+Tag.seed do |t|
+  t.name = "離散系論"
+end
+
+Tag.seed do |t|
+  t.name = "データマネジメント概論"
+end
+
+Tag.seed do |t|
+  t.name = "コンピュータ理工学演習I"
+end
+
+Tag.seed do |t|
+  t.name = "コンピュータ理工学演習II"
+end
+
+Tag.seed do |t|
+  t.name = "コンピュータアーキテクチャ論"
+end
+
+Tag.seed do |t|
+  t.name = "論理回路設計論"
+end
+
+Tag.seed do |t|
+  t.name = "オートマトンと言語理論"
+end
+
+Tag.seed do |t|
+  t.name = "ソフトウェア工学概論"
+end
+
+Tag.seed do |t|
+  t.name = "コンピュータネットワーク概論"
+end
+
+Tag.seed do |t|
+  t.name = "大学受験"
+end
+
+Tag.seed do |t|
+  t.name = "会津大学"
+end
+
+Tag.seed do |t|
+  t.name = "TOEIC"
+end
+
+Tag.seed do |t|
+  t.name = "応用代数"
+end
+
+Tag.seed do |t|
+  t.name = "複素関数論"
+end
+
+Tag.seed do |t|
+  t.name = "量子力学"
+end
+
+Tag.seed do |t|
+  t.name = "半導体デバイス"
+end
+
+Tag.seed do |t|
+  t.name = "熱・統計力学"
+end
+
+Tag.seed do |t|
+  t.name = "プログラミングJava II"
+end
+
+Tag.seed do |t|
+  t.name = "コンピュータグラフィックス論"
+end
+
+Tag.seed do |t|
+  t.name = "信号処理と線形システム"
+end
+
+Tag.seed do |t|
+  t.name = "ビジュアルコンピューティングのための幾何学"
+end
+
+Tag.seed do |t|
+  t.name = "システム総合演習I"
+end
+
+Tag.seed do |t|
+  t.name = "ソフトウェア総合演習I"
+end
+
+Tag.seed do |t|
+  t.name = "研究室"
+end
+
+Tag.seed do |t|
+  t.name = "概論"
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-User.create(email: "test@test.com", password_digest:"testtest")
-
-Tag.create(name: "testTag1")
-Tag.create(name: "testTag2")
-Tag.create(name: "testTag3")
-Tag.create(name: "testTag4")
-Tag.create(name: "testTag5")
-
-Tagging.create(question_id: 1, tag_id: 1)
-Tagging.create(question_id: 2, tag_id: 2)
-Tagging.create(question_id: 3, tag_id: 3)
-Tagging.create(question_id: 4, tag_id: 4)
-Tagging.create(question_id: 1, tag_id: 2)
