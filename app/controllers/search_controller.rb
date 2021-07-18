0238f31754359f869ebb65b40630675fb57621f1
a-kit-2 <akito.nakamura.c@gmail.com>
2021-07-18 13:49:01 +0900

    検索の際にいいねなどが表示されない現象修正

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
   #文字列検索
   def searchByWord
     word = params[:word]
-    questions = Question.where('title like ? OR content like ?', "%#{word}%", "%#{word}%").order(created_at: "DESC")
+    questions = Question.joins(:user, :like).select('questions.*, users.id as user_id, users.name as user_name, likes.count as like_count, likes.id as like_id').where('title like ? OR content like ?', "%#{word}%", "%#{word}%").order(created_at: "DESC")
     if questions.length > 0
       render json: {"questions"=>questions, "searched_by_word"=>true}
     else
@@ -15,7 +15,7 @@ class SearchController < ApplicationController
     #/tags/2/search の場合、id２のタグ取得
     tag = Tag.find(params[:id]) 
     #tagに関連するquestionを全取得
-    questions = tag.questions.all
+    questions = tag.questions.joins(:user, :like).select('questions.*, users.id as user_id, users.name as user_name, likes.count as like_count, likes.id as like_id').all
     p questions
     if questions.length > 0
       render json: {"questions" => questions, "get_question" => true}
